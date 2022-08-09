f143572600eacf8929bb96805dd6baceae71b4a4
erikamikami <g.erika3up@gmail.com>
2022-08-09 16:38:38 +0900

    ItemRepository作成

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -1,6 +1,25 @@
 package com.example.repository;
 
+import java.util.List;
 
-public class ItemRepository {
+import org.apache.ibatis.annotations.Mapper;
 
+import com.example.pagination.Pagination;
+
+@Mapper
+public interface ItemRepository {
+	
+	/**
+	 * itemテーブルの総レコード数を取得
+	 * @return
+	 */
+	public int countAllItems();
+	
+	/**
+	 * itemを全て取得（ページングあり）
+	 * @param pagination
+	 * @return
+	 */
+	public List<Item> findAll(Pagination pagination);
+	
 }
