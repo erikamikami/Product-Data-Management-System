7d271d2a4a0c4212d718c82cf036c7a11655ad06
erikamikami <g.erika3up@gmail.com>
2022-08-12 15:05:23 +0900

    item追加機能作成

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -3,11 +3,16 @@ package com.example.controller;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.entity.Item;
 import com.example.form.ItemForm;
@@ -60,6 +65,29 @@ public class ItemController {
 		Set<String> parentCategoryList = categoryService.getParentCategoryList();
 		model.addAttribute("parentCategoryList", parentCategoryList);
 		return "add";
+	}
+	
+	/**
+	 * item新規追加
+	 * @return
+	 */
+	@RequestMapping("/add/comp")
+	public String itemAdd(@Validated ItemForm itemForm, BindingResult result, RedirectAttributes redirectAttribute) {
+		if(result.hasErrors()) {
+			redirectAttribute.addFlashAttribute(itemForm);
+			redirectAttribute.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + Conventions.getVariableName(itemForm), result);
+			System.out.println(result);
+			return "redirect:/item/add";
+		}
+		
+		itemForm.setCategory(itemForm.getParentCategory(), itemForm.getChildCategory(), itemForm.getGrandChild());
+		Item item = new Item();
+		BeanUtils.copyProperties(itemForm, item);
+		item.setPrice(Double.parseDouble(itemForm.getPrice()));
+		
+		itemService.register(item);
+		
+		return "redirect:/item/list";
 	}
 
 }
--- src/main/java/com/example/form/ItemForm.java
+++ src/main/java/com/example/form/ItemForm.java
@@ -1,5 +1,9 @@
 package com.example.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,15 +15,33 @@ import lombok.ToString;
 public class ItemForm {
 	
 	private int id;
+	
+	@NotBlank(message = "error:may not be empty")
 	private String name;
-	private int condition;
+
+	@NotNull(message = "error:may not be empty")
+	private Integer condition;
+	
 	private String category;
+
+	@NotBlank(message = "error:may not be empty")
 	private String parentCategory;
+
+	@NotBlank(message = "error:may not be empty")
 	private String childCategory;
+
 	private String grandChild;
+
+	@NotBlank(message = "error:may not be empty")
 	private String brandName;
-	private double price;
-	private int shipping;
+	
+	@NotBlank(message = "error:may not be empty")
+	private String price;
+	
+	@NotNull(message = "error:may not be empty")
+	private Integer shipping;
+	
+	@NotBlank(message = "error:may not be empty")
 	private String itemDescription;
 	
 	
@@ -35,11 +57,11 @@ public class ItemForm {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getCondition() {
+	public Integer getCondition() {
 		return condition;
 	}
-	public void setCondition(String condition) {
-		this.condition = Integer.parseInt(condition);
+	public void setCondition(Integer condition) {
+		this.condition = condition;
 	}
 	public String getCategory() {
 		return category;
@@ -71,17 +93,17 @@ public class ItemForm {
 	public void setBrandName(String brandName) {
 		this.brandName = brandName;
 	}
-	public double getPrice() {
+	public String getPrice() {
 		return price;
 	}
-	public void setPrice(double price) {
+	public void setPrice(String price) {
 		this.price = price;
 	}
-	public int getShipping() {
+	public Integer getShipping() {
 		return shipping;
 	}
-	public void setShipping(String shipping) {
-		this.shipping = Integer.parseInt(shipping);
+	public void setShipping(Integer shipping) {
+		this.shipping = shipping;
 	}
 	public String getItemDescription() {
 		return itemDescription;
